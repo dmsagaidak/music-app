302090b41b4e8bd41c3f0c7365b8f535d5f0596b
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2023-02-15 00:33:41 +0600

    Created route and method for user authentification

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,13 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, {Model} from "mongoose";
 import {IUser} from "../types";
 import bcrypt from 'bcrypt';
 
 
 const SALT_WORK_FACTOR = 10;
 
+interface IUserMethods {
+    checkPassword(password: string): Promise<boolean>;
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>;
+
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema<IUser>({
+const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
     username: {
         type: String,
         required: true,
@@ -23,12 +29,21 @@ UserSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
 
     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-    const hash = await bcrypt.hash(this.password, salt);
-
-    this.password = hash;
+    this.password = await bcrypt.hash(this.password, salt);
 
     next();
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.set('toJSON', {
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+UserSchema.methods.checkPassword = function(password) {
+    return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model<IUser, UserModel>('User', UserSchema);
 export default User;
--- api/routers/users.ts
+++ api/routers/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const usersRouter = express.Router();
 
@@ -21,6 +22,22 @@ usersRouter.post('/', async (req, res, next) => {
         return next(e);
     }
 });
+
+usersRouter.post('/sessions', async (req, res) => {
+    const user = await User.findOne({username: req.body.username});
+
+    if(!user) {
+        return res.status(400).send({error: 'User not found'});
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if(!isMatch){
+        return res.status(400).send({error: 'Password not found'});
+    }
+
+    return res.send({message: 'Username and password are correct'});
+})
 
 
 
